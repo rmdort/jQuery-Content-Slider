9af50aea7c304aaafa90667e61f450b89d4ba0b8
Vinay <rmdort@gmail.com>
2011-01-31 00:36:43 +0800

    Added pagination support

--- jquery.content.slider.js
+++ jquery.content.slider.js
@@ -6,7 +6,10 @@
     var settings={
       pager:'.pager',
       autoplay:true,
-      delay:5000
+      delay:5000,
+      pagerthumbs:false,
+      perpage:5,
+      pagerid:'#Thumbs'
     };
     
     if(options) $.extend(settings,options)
@@ -22,65 +25,149 @@
   	    	pageli='',
   	    	imageWidth = $slideshow.width(),
           imageSum = $slideimages.size(),
-          imageReelWidth = imageWidth * imageSum;
+          imageReelWidth = imageWidth * imageSum,
+          imageReelHeight =$slideimages.height();
       
-      $slidereel.css({
-        width:imageReelWidth,
-        position:"relative"
-        });
+          $slidereel.css({
+            width:imageReelWidth,
+            height:imageReelHeight,
+            position:"relative"
+            });
   	  
-  	  // Append Page Numbers
+      	  // Append Page Numbers
   	  
-  	  $slideimages.each(function(i){
-  	    pageli += '<li><a href="#">'+(i+1)+'</a></li>';  	      	    
-  	  });  	  
+      	  $slideimages.each(function(i){
+      	    pageli += '<li><a href="#">'+(i+1)+'</a></li>';  	      	    
+      	  });  	  
   	  
-  	  $(settings.pager).append(pageli).find('li:first').addClass("active").end()
-  	  .find("li").live('click',function(e) {	
-    		$active = $(this); 
-    		if(settings.autoplay) clearInterval(play); 
-    		rotate(); 
-    		rotateSwitch(); 
-    		e.preventDefault();
-    		
-    	});;
+      	  if(!settings.pagerthumbs){
+      	    $(settings.pager).append(pageli).find('li:first').addClass("active").end()
+        	  .find("li").live('click',function(e) {	
+          		$active = $(this); 
+          		if(settings.autoplay) clearInterval(play); 
+          		rotate(); 
+          		if(settings.autoplay) rotateSwitch();
+          		e.preventDefault();
+          	});
+          	
+      	  }
+      	  else{
+            paginate(settings.pagerid,settings.perpage);
+            startshow();
+      	  }
   	  
   	  
-  	  //Paging + Slider Function
-    	rotate = function(){	
-    		var triggerID = $active.text() - 1; //Get number of times to slide
-    		var image_reelPosition = triggerID * imageWidth; //Determines the distance the image reel needs to slide
+  	  
+      	  //Paging + Slider Function
+        	rotate = function(){	
+        		var triggerID = $active.index(); //Get number of times to slide
+        		var image_reelPosition = triggerID * imageWidth; //Determines the distance the image reel needs to slide
 
-    		$(settings.pager).find("li").removeClass('active'); //Remove all active class
-    		$active.addClass('active'); //Add active class (the $active is declared in the rotateSwitch function)
+        		$(settings.pager).find("li").removeClass('active'); //Remove all active class
+        		$active.addClass('active'); //Add active class (the $active is declared in the rotateSwitch function)
 
-    		//Slider Animation
-    		$slidereel.animate({ 
-    			left: -image_reelPosition
-    		}, 500 );
+        		//Slider Animation
+        		$slidereel.animate({ 
+        			left: -image_reelPosition
+        		}, 500);
         
-    	}; 
+        	}; 
     	
-    	//Rotation + Timing Event
-    	rotateSwitch = function(){	
-    		play = setInterval(function(){ 
-    			$active = $(settings.pager).find('.active').next();
-    			if ( $active.length === 0) { //If paging reaches the end...
-    				$active = $(settings.pager).find('li:first'); //go back to first
-    			}
-    			rotate(); 
-    		}, settings.delay); 
-    	};
+        	//Rotation + Timing Event
+        	rotateSwitch = function(){	
+        		play = setInterval(function(){ 
+        			$active = $(settings.pager).find('.active').next();
+        			if ( $active.length === 0) { //If paging reaches the end...
+        				$active = $(settings.pager).find('li:first'); //go back to first
+        			}
+        			rotate(); 
+        		}, settings.delay); 
+        	};
       
-      if(settings.autoplay) rotateSwitch();
+          if(settings.autoplay) rotateSwitch();
             
-      //On Hover
-      	$slideshow.hover(function() {
-      		clearInterval(play); //Stop the rotation
-      	}, function() {
-      		rotateSwitch(); //Resume rotation
-      	});
+          //On Hover
+
+        	$slideshow.hover(function() {
+        		if(settings.autoplay) clearInterval(play); //Stop the rotation
+        	}, function() {
+        		if(settings.autoplay) rotateSwitch(); //Resume rotation
+        	});
   	  
+  	  
+      	  // Pagination
+  	      
+      	  function paginate(id,perpage){
+      	    $id = $(id);
+      	    $items =$id.find('li');
+      	    var numPages = Math.ceil($items.length / perpage) - 1;
+      	    var current = 0;
+  	    
+      	    var $next = $('#Next');
+      	    var $back = $('#Prev');
+  	    
+      	    $back
+              .addClass('paging-disabled')
+              .click(function(e) {
+                pagination('<');
+                e.preventDefault();
+              });
+          
+            $next.click(function(e) {
+              pagination('>');
+              e.preventDefault();
+            });
+        
+            $items
+              .hide()
+              .click(function(e){
+                i = $(this).index();
+                $slideimages.hide().eq(i).show();
+                $items.removeClass("active").eq(i).addClass("active");
+                e.preventDefault();
+              })
+              .slice(0,perpage)
+              .show();
+              
+            
+              pagination = function (direction){
+                reveal = function (current){
+                  $back.removeClass('paging-disabled');
+                  $next.removeClass('paging-disabled');
+
+                  $items
+                    .hide()
+                    .slice(current * perpage, current * perpage + perpage)
+                    .fadeIn(300);
+
+                };
+                // 4. Move previous and next  
+              	if (direction == '<') { // previous
+                  if (current > 1) {
+                    reveal(current -= 1);
+                  }
+                  else if (current == 1) {
+                    reveal (current -= 1);
+                    $back.addClass('paging-disabled');
+                  }
+                } else {
+                  if (current < numPages - 1) {
+                    reveal(current += 1);
+                  }
+                  else if (current == numPages - 1) {
+                    reveal(current += 1);
+                    $next.addClass('paging-disabled');
+                  }
+                }
+              }
+              
+          
+      	  };
+      	  
+      	  function startshow(){
+      	    $slideimages.hide().eq(0).show();
+      	    $items.eq(0).addClass("active");
+      	  };
   	  
   	});
   	
